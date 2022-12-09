560193be68e353f60e28fd49c5e62864875dd052
Léo Peyronnet <leo.peyronnet@outlook.fr>
2022-12-09 22:37:22 +0100

    Create SynethiaManager.cs

---
+++ Synethia/Synethia/SynethiaManager.cs
@@ -0,0 +1,128 @@
+﻿/*
+MIT License
+
+Copyright (c) Léo Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Synethia
+{
+	public static class SynethiaManager
+	{
+		public static SynethiaConfig Load(string path)
+		{
+			SynethiaConfig config = new();
+			if (!File.Exists(path)) // If no Synethia config exists
+			{
+				string json = JsonSerializer.Serialize(config, new JsonSerializerOptions { WriteIndented = true });
+				File.WriteAllText(path, json);
+				return new();
+			}
+
+			// If Synethia config exists
+			// Deserialize the file to Synethia config (using JSON)
+			return JsonSerializer.Deserialize<SynethiaConfig>(File.ReadAllText(path)) ?? new();
+		}
+
+		public static void Save(SynethiaConfig synethiaConfig, string path)
+		{
+			string json = JsonSerializer.Serialize(synethiaConfig, new JsonSerializerOptions { WriteIndented = true });
+			File.WriteAllText(path, json);
+		}
+
+		public static void InjectSynethiaCode(Page page, ref PageInfo pageInfo, bool code)
+		{
+			PageInfo info = pageInfo;
+			page.Loaded += (o, e) =>
+			{
+				if (!code)
+				{
+					code = true;
+					// For each button of the page
+					foreach (Button b in FindVisualChildren<Button>(page))
+					{
+						b.Click += (sender, e) =>
+						{
+							info.InteractionCount++;
+						};
+					}
+
+					// For each TextBox of the page
+					foreach (TextBox textBox in FindVisualChildren<TextBox>(page))
+					{
+						textBox.GotFocus += (o, e) =>
+						{
+							info.InteractionCount++;
+						};
+					}
+
+					// For each CheckBox/RadioButton of the page
+					foreach (CheckBox checkBox in FindVisualChildren<CheckBox>(page))
+					{
+						checkBox.Checked += (o, e) =>
+						{
+							info.InteractionCount++;
+						};
+						checkBox.Unchecked += (o, e) =>
+						{
+							info.InteractionCount++;
+						};
+					}
+
+					foreach (RadioButton radioButton in FindVisualChildren<RadioButton>(page))
+					{
+						radioButton.Checked += (o, e) =>
+						{
+							info.InteractionCount++;
+						};
+						radioButton.Unchecked += (o, e) =>
+						{
+							info.InteractionCount++;
+						};
+					}
+				}
+			};
+
+			pageInfo = info;
+		}
+
+		private static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+		{
+			if (depObj == null) yield return (T)Enumerable.Empty<T>();
+			for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+			{
+				DependencyObject ithChild = VisualTreeHelper.GetChild(depObj, i);
+				if (ithChild == null) continue;
+				if (ithChild is T t) yield return t;
+				foreach (T childOfChild in FindVisualChildren<T>(ithChild)) yield return childOfChild;
+			}
+		}
+	}
+}
